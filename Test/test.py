0fa1440cce7f4c0a36b3cb22ddee4fdce4b3d425
Lokesh Sharma <lokeshsharma2009@gmail.com>
2019-09-15 12:43:35 +0530

    First selenium framework open url in python

---
+++ Test/test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("D:/Drivers/chromedriver_win32/chromedriver.exe")
+driver.set_page_load_timeout(20)
+driver.get("http://www.gmail.com")
+driver.maximize_window()
+driver.imlicitly_wait(10)
+
+
