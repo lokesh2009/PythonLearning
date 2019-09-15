4634e42e5b3fe836750932853c3a41f941b0c9f5
Lokesh Sharma <lokeshsharma2009@gmail.com>
2019-09-15 13:07:58 +0530

    Screenshot added

--- Test/test.py
+++ Test/test.py
@@ -4,6 +4,8 @@ driver = webdriver.Chrome("D:/Drivers/chromedriver_win32/chromedriver.exe")
 driver.set_page_load_timeout(20)
 driver.get("http://www.gmail.com")
 driver.maximize_window()
-driver.imlicitly_wait(10)
+driver.implicitly_wait(20)
+
+driver.get_screenshot_as_file("C:/Users/lsharma/Desktop/chromedriver_win32/Home.jpeg")
 driver.close()
 
