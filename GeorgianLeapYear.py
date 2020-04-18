7131a71ed8fdbad4f2458f57b431e4508d7511e5
Lokesh <lokeshsharma2009@gmail.com>
2020-04-18 13:22:23 +0530

    Puzzle detail added for Georgianleap year

--- GeorgianLeapYear.py
+++ GeorgianLeapYear.py
@@ -1,5 +1,11 @@
 import calendar
+# This is the Georgian calender puzzle to show up the leap year or not
+#We add a Leap Day on February 29, almost every four years. The leap day is an extra, or intercalary day and we add it to the shortest month of the year, February.
+#In the Gregorian calendar three criteria must be taken into account to identify leap years:
 
+#The year can be evenly divided by 4, is a leap year, unless:
+#The year can be evenly divided by 100, it is NOT a leap year, unless:
+#The year is also evenly divisible by 400. Then it is a leap year
 
 def is_leap(year):
     if year % 4 == 0:
