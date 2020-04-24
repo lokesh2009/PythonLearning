51f0f681effe05524a6187cace59da346677dbfd
lsharma <lsharma@WBMI.com>
2020-04-24 10:33:38 +0530

    "This is for if and elif and else concept"

---
+++ LOCTrue.py
@@ -0,0 +1,11 @@
+loc = 'Auto shop'
+if loc == 'Auto shop':
+    print("Car s cool")
+elif loc == 'Bank':
+    print("Money is cool")
+elif loc == 'Store':
+    print("Welcome to the store")
+else:
+    print("I do not know much on this")
+print("**************************************")
+print("******For Loop")
