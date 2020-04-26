9b3c95cb6004c1bf97e6ea77e88ccab314a698ce
lsharma <lsharma@WBMI.com>
2020-04-26 12:42:18 +0530

    "This is for AnagramPrograme commit"

--- CoronaVirusPYscript.py
+++ CoronaVirusPYscript.py
@@ -40,7 +40,7 @@ y_pos = np.arange(len(objects))
 
 performance = []
 for row in stats:
-    performance.append(int(row[2]) + int(row[3]))
+    performance.append((row[2]) + (row[3]))
 
 table = tabulate(stats, headers=SHORT_HEADERS)
 print(table)
