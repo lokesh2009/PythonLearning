3909a1893f5a5ea2171f1189ac499bdc072095d4
lsharma <lsharma@WBMI.com>
2020-04-26 12:42:40 +0530

    "This is for Anagram Program commit"

---
+++ ToFindNoofKeyword.py
@@ -0,0 +1,15 @@
+import keyword
+
+
+keys=["for", "while", "tanisha", "break", "sky","elif", "assert", "pulkit", "lambda", "else", "sakshar"]
+
+for i in range(len(keys)):
+    if keyword.iskeyword(keys[i]):
+        print(keys[i]+"  is keyword")
+    else:
+        print(keys[i]+"IS not key word")
+
+# How to print all keys
+
+print("The list of keyword is :")
+print(keyword.kwlist)
